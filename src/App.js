8bc7679a745f7edbec06ac7679263707c1e4f7ba
arpit2707 <arpitgaurav.goldi723@gmail.com>
2023-07-12 14:42:19 +0530

    baseReadyLogin

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <label className="sidebar">Board</label>
+        </div>
+        <div>
+          <label className="signInLabel-1">Sign In</label>
+          <label className="signInLabel-1">Sign in to your account</label>
+          <button>Sign in with Google</button>
+          <button>Sign in with Apple</button>
+          <label>Email Address</label>
+          <input type="email"></input>
+          <label>Password</label>
+          <input type="password"></input>
+          <label>Forgot Password?</label>
+          <button>Sign In</button>
+          <label>
+            Don't have an account?{" "}
+            <a href="https://google.com">Register here</a>
+          </label>
+        </div>
       </header>
     </div>
   );
