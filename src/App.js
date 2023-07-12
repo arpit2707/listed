c0417502ee56ec1bd7d18f35aeb0d7d4d23a074b
arpit2707 <arpitgaurav.goldi723@gmail.com>
2023-07-12 18:34:49 +0530

    Design Updated

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import "./App.css";
+import { FaAlignCenter, FaGoogle, FaApple } from "react-icons/fa";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div>
-          <label className="sidebar">Board</label>
+      <div className="sidebar">
+        <label className="sideBoard">Board</label>
+      </div>
+      <div className="loginGrid">
+        <label className="signInLabel-1">Sign In</label>
+        <label className="signInLabel-2">Sign in to your account</label>
+        <div className="authButtons">
+          <button src="" className="gAuth">
+            <FaGoogle style={FaAlignCenter} /> Sign in with Google
+          </button>
+          <button className="appleAuth">
+            {" "}
+            <FaApple /> Sign in with Apple
+          </button>
         </div>
-        <div>
-          <label className="signInLabel-1">Sign In</label>
-          <label className="signInLabel-1">Sign in to your account</label>
-          <button>Sign in with Google</button>
-          <button>Sign in with Apple</button>
-          <label>Email Address</label>
-          <input type="email"></input>
-          <label>Password</label>
-          <input type="password"></input>
-          <label>Forgot Password?</label>
-          <button>Sign In</button>
-          <label>
-            Don't have an account?{" "}
-            <a href="https://google.com">Register here</a>
-          </label>
+        <div className="loginSection">
+          <label className="emailLabel">Email Address</label>
+          <input className="emailInput" type="email"></input>
+          <label className="passwordLabel">Password</label>
+          <input className="passwordInput" type="password"></input>
+          <a href="https://google.com">Forgot Password?</a>
+          <button className="signIn">Sign In</button>
         </div>
-      </header>
+        <label>Don't have an account?</label>
+        <a href="https://google.com">Register here</a>
+      </div>
     </div>
   );
 }
